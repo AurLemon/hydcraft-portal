78717535fb8724368cb59dbc7450ad9ce3c430fc
AurLemon <aurlemoncn@gmail.com>
2026-06-08 23:07:35 +0800

    ci: 调整 CI/CD 测试 Build 流

--- server/plugins/portal-bridge.ts
+++ server/plugins/portal-bridge.ts
@@ -1,5 +1,12 @@
 import { portalBridgeManager } from '../utils/portal-bridge/client'
 
 export default defineNitroPlugin(() => {
+	if (
+		import.meta.prerender ||
+		process.env.HYDCRAFT_DISABLE_PORTAL_BRIDGE === '1'
+	) {
+		return
+	}
+
 	void portalBridgeManager.startEnabled()
 })
